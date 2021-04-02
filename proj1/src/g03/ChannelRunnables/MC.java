c9589732d426282fdcaa652919482d8eef8ad609
up201806512 <up201806512@fe.up.pt>
2021-04-02 16:44:15 +0100

    Changed channel operations to threads

--- proj1/src/g03/ChannelRunnables/MC.java
+++ proj1/src/g03/ChannelRunnables/MC.java
@@ -4,6 +4,7 @@ import g03.*;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Random;
 import java.util.concurrent.ScheduledFuture;
@@ -22,63 +23,137 @@ public class MC implements Runnable {
         while (true) {
             try {
                 Message message = new Message(peer.getMC().receive());
-                if (message.getType() == MessageType.STORED) {
-                    String key = message.getFileId() + "-" + message.getChunkNumber();
+                //TODO: refactor - maybe change this to runnable classes
+                switch(message.getType()) {
+                    case STORED:
+                        new Thread(() -> {
+                            String key = message.getFileId() + "-" + message.getChunkNumber();
 
-                    if (peer.getChunks().containsKey(key)) {
-                        Chunk c = peer.getChunks().get(key);
-                        c.getPeers().add(message.getSenderId());
-                    } else { //IDK what this else means
-                        Chunk c = new Chunk(message.getFileId(), message.getChunkNumber(), message.getReplicationDegree());
-                        peer.getChunks().put(key, c);
-                    }
+                            if (peer.getChunks().containsKey(key)) {
+                                Chunk c = peer.getChunks().get(key);
+                                c.getPeers().add(message.getSenderId());
+                            } else { //IDK what this else means
+                                Chunk c = new Chunk(message.getFileId(), message.getChunkNumber(), message.getReplicationDegree());
+                                peer.getChunks().put(key, c);
+                            }
+                        }).start();
+                        break;
 
-                } else if (message.getType() == MessageType.GETCHUNK) {
-                    String key = message.getFileId() + "-" + message.getChunkNumber();
-                    if (peer.getChunks().containsKey(key)) {
-                        String[] msgArgs = {peer.getProtocolVersion(),
-                                String.valueOf(peer.getId()),
-                                message.getFileId(),
-                                String.valueOf(message.getChunkNumber())};
+                    case GETCHUNK:
+                        new Thread(() -> {
+                            String key = message.getFileId() + "-" + message.getChunkNumber();
+                            if (peer.getChunks().containsKey(key)) {
+                                String[] msgArgs = {peer.getProtocolVersion(),
+                                        String.valueOf(peer.getId()),
+                                        message.getFileId(),
+                                        String.valueOf(message.getChunkNumber())};
 
-                        //TODO: refactor
-                        byte[] body;
-                        try (FileInputStream file = new FileInputStream(key)) {
-                            body = file.readAllBytes();
-                        }
+                                //TODO: refactor
+                                byte[] body = null;
+                                try (FileInputStream file = new FileInputStream(key)) {
+                                    body = file.readAllBytes();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
 
-                        //Schedule the CHUNK message
-                        Message msgToSend = new Message(MessageType.CHUNK, msgArgs, body);
-                        ScheduledFuture<?> task = peer.getPool().schedule(new ChunkMessageSender(peer, msgToSend), new Random().nextInt(400), TimeUnit.MILLISECONDS);
-                        peer.getMessagesToSend().put(key, task);
-                    }
-                } else if (message.getType() == MessageType.DELETE) {
-                    peer.getChunks().forEach((key, value) -> {
-                        if (key.startsWith(message.getFileId()) && peer.getChunks().remove(key, value)) {
-                            File chunkToDelete = new File(key);
-                            chunkToDelete.delete();
-                        }
-                    });
-                } else if (message.getType() == MessageType.REMOVED) {
-                    String key = message.getFileId() + "-" + message.getChunkNumber();
-                    if (peer.getChunks().containsKey(key)) {
-                        peer.getChunks().get(key).getPeers().remove(message.getSenderId());
+                                //Schedule the CHUNK message
+                                Message msgToSend = new Message(MessageType.CHUNK, msgArgs, body);
+                                ScheduledFuture<?> task = peer.getPool().schedule(new ChunkMessageSender(peer, msgToSend), new Random().nextInt(400), TimeUnit.MILLISECONDS);
+                                peer.getMessagesToSend().put(key, task);
+                            }
+                        }).start();
+                        break;
+
+                    case DELETE:
+                        new Thread(() -> peer.getChunks().forEach((key, value) -> {
+                            if (key.startsWith(message.getFileId()) && peer.getChunks().remove(key, value)) {
+                                File chunkToDelete = new File(key);
+                                chunkToDelete.delete();
+                            }
+                        })).start();
+                        break;
+
+                    case REMOVED:
+                        new Thread(() -> {
+                            String key = message.getFileId() + "-" + message.getChunkNumber();
+                            if (peer.getChunks().containsKey(key)) {
+                                peer.getChunks().get(key).getPeers().remove(message.getSenderId());
 
 
-                        //TODO: check if replication degree drops below desired (Kinda done)
-                        if (peer.getChunks().get(key).getPerceivedReplicationDegree() < peer.getChunks().get(key).getDesiredReplicationDegree()) {
-                            String[] msgArgs = {peer.getProtocolVersion(),
-                                    String.valueOf(peer.getId()),
-                                    message.getFileId(),
-                                    String.valueOf(message.getChunkNumber())};
-                            Message msgToSend = new Message(MessageType.PUTCHUNK, msgArgs, null);
-                            ScheduledFuture<?> task = peer.getPool().schedule(
-                                    new PutChunkMessageSender(peer, msgToSend, peer.getChunks().get(key).getDesiredReplicationDegree(), 5),
-                                    new Random().nextInt(400), TimeUnit.MILLISECONDS);
-                            peer.getBackupsToSend().put(key, task);
-                        }
-                    }
+                                //TODO: check if replication degree drops below desired (Kinda done)
+                                if (peer.getChunks().get(key).getPerceivedReplicationDegree() < peer.getChunks().get(key).getDesiredReplicationDegree()) {
+                                    String[] msgArgs = {peer.getProtocolVersion(),
+                                            String.valueOf(peer.getId()),
+                                            message.getFileId(),
+                                            String.valueOf(message.getChunkNumber())};
+                                    Message msgToSend = new Message(MessageType.PUTCHUNK, msgArgs, null);
+                                    ScheduledFuture<?> task = peer.getPool().schedule(
+                                            new PutChunkMessageSender(peer, msgToSend, peer.getChunks().get(key).getDesiredReplicationDegree(), 5),
+                                            new Random().nextInt(400), TimeUnit.MILLISECONDS);
+                                    peer.getBackupsToSend().put(key, task);
+                                }
+                            }
+                        }).start();
+                        break;
+
                 }
+//                if (message.getType() == MessageType.STORED) {
+//                    String key = message.getFileId() + "-" + message.getChunkNumber();
+//
+//                    if (peer.getChunks().containsKey(key)) {
+//                        Chunk c = peer.getChunks().get(key);
+//                        c.getPeers().add(message.getSenderId());
+//                    } else { //IDK what this else means
+//                        Chunk c = new Chunk(message.getFileId(), message.getChunkNumber(), message.getReplicationDegree());
+//                        peer.getChunks().put(key, c);
+//                    }
+//
+//                } else if (message.getType() == MessageType.GETCHUNK) {
+//                    String key = message.getFileId() + "-" + message.getChunkNumber();
+//                    if (peer.getChunks().containsKey(key)) {
+//                        String[] msgArgs = {peer.getProtocolVersion(),
+//                                String.valueOf(peer.getId()),
+//                                message.getFileId(),
+//                                String.valueOf(message.getChunkNumber())};
+//
+//                        //TODO: refactor
+//                        byte[] body;
+//                        try (FileInputStream file = new FileInputStream(key)) {
+//                            body = file.readAllBytes();
+//                        }
+//
+//                        //Schedule the CHUNK message
+//                        Message msgToSend = new Message(MessageType.CHUNK, msgArgs, body);
+//                        ScheduledFuture<?> task = peer.getPool().schedule(new ChunkMessageSender(peer, msgToSend), new Random().nextInt(400), TimeUnit.MILLISECONDS);
+//                        peer.getMessagesToSend().put(key, task);
+//                    }
+//                } else if (message.getType() == MessageType.DELETE) {
+//                    peer.getChunks().forEach((key, value) -> {
+//                        if (key.startsWith(message.getFileId()) && peer.getChunks().remove(key, value)) {
+//                            File chunkToDelete = new File(key);
+//                            chunkToDelete.delete();
+//                        }
+//                    });
+//                } else if (message.getType() == MessageType.REMOVED) {
+//                    String key = message.getFileId() + "-" + message.getChunkNumber();
+//                    if (peer.getChunks().containsKey(key)) {
+//                        peer.getChunks().get(key).getPeers().remove(message.getSenderId());
+//
+//
+//                        //TODO: check if replication degree drops below desired (Kinda done)
+//                        if (peer.getChunks().get(key).getPerceivedReplicationDegree() < peer.getChunks().get(key).getDesiredReplicationDegree()) {
+//                            String[] msgArgs = {peer.getProtocolVersion(),
+//                                    String.valueOf(peer.getId()),
+//                                    message.getFileId(),
+//                                    String.valueOf(message.getChunkNumber())};
+//                            Message msgToSend = new Message(MessageType.PUTCHUNK, msgArgs, null);
+//                            ScheduledFuture<?> task = peer.getPool().schedule(
+//                                    new PutChunkMessageSender(peer, msgToSend, peer.getChunks().get(key).getDesiredReplicationDegree(), 5),
+//                                    new Random().nextInt(400), TimeUnit.MILLISECONDS);
+//                            peer.getBackupsToSend().put(key, task);
+//                        }
+//                    }
+//                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
